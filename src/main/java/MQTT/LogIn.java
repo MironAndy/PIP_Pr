0aafd8a18257eb89deda71936006c0fb782666cd
MironAndy <andrei.miron@student.tuiasi.ro>
2022-04-28 18:14:51 +0300

    adaugat prototip broker

---
+++ src/main/java/MQTT/LogIn.java
@@ -0,0 +1,49 @@
+package MQTT;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class LogIn implements MqttCallback {
+	
+	MqttClient client;
+
+	public LogIn() {
+	}
+
+	public static void main(String[] args) {
+	    new LogIn().doDemo("Andrei");
+	}
+
+	public void doDemo(String name) {
+	    try {
+	        client = new MqttClient("tcp://127.0.0.1:8081", name);
+	        client.connect();
+	        client.setCallback(this);
+	        client.subscribe("foo");
+	        MqttMessage message = new MqttMessage();
+	        message.setPayload("A single message from my computer fff".getBytes());
+	        client.publish("foo", message);
+	    } catch (MqttException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	public void connectionLost(Throwable cause) {
+	    // TODO Auto-generated method stub
+
+	}
+
+	public void messageArrived(String topic, MqttMessage message)
+	        throws Exception {
+	 System.out.println(message);
+	}
+
+	public void deliveryComplete(IMqttDeliveryToken token) {
+	    // TODO Auto-generated method stub
+
+	}
+
+}
