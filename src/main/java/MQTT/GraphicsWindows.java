73233569c1df555b2323c1aba24769f986268c60
MironAndy <andrei.miron@student.tuiasi.ro>
2022-05-21 14:50:32 +0300

    Adaugare client

--- src/main/java/MQTT/GraphicsWindows.java
+++ src/main/java/MQTT/GraphicsWindows.java
@@ -26,9 +26,11 @@ public class GraphicsWindows {
 	
 	public JFrame windows1 = new JFrame("Log In");
 	public JFrame windows2 = new JFrame("Main Application");
+	
 	ActionButton b = new ActionButton();
 	String broker = "tcp://mqtt.eclipseprojects.io:1883";
     int qos = 0;
+    
     //String clientId = "name1 Client";
     MemoryPersistence persistence = new MemoryPersistence();
     MqttClient mqttClient;
@@ -42,29 +44,31 @@ public class GraphicsWindows {
     JTextField[] t = new JTextField[100];
     JTextField nickname;
     JPasswordField password2;
+    
 	GraphicsWindows() {
 		initializeWindows1();
 	}
-	void initializeWindows1() {
-		windows1.setBounds(800,300,400,300);
-		windows1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		windows1.getContentPane().setBackground((new Color(100,200,225)));
+	
+	void initNickname() {
 		
 		nickname = new JTextField(" Nickname: "); 
 		nickname.setEditable(true);
 		nickname.setBounds(60,30,70,20);
-		windows1.getContentPane().add(nickname);
+		
 		nickname.setVisible(true); 
 		nickname.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
 				nickname.setText(" ");
 			}
 		});
+		windows1.getContentPane().add(nickname);
+	}
+	
+	void initPasswordButton() {
 		
 		final JTextField password = new JTextField(" Password: "); 
 		password.setEditable(true);
 		password.setBounds(150,30,70,20);
-		windows1.getContentPane().add(password);
 		password.setVisible(true);
 		password.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
@@ -76,138 +80,184 @@ public class GraphicsWindows {
 				password2.setVisible(true);
 			}
 		});
+		windows1.getContentPane().add(password);
+	}
+	
+	void initLoginButton() {
 		
 		JButton logIn_button = new JButton("Log in"); 
 		logIn_button.setBounds(250, 30, 80, 20);
-		windows1.getContentPane().add(logIn_button);
 		logIn_button.setVisible(true);
 		logIn_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				windows1.setVisible(false);
-				initializeWindows2();
+				if (!nickname.getText().isBlank() && password2.getPassword() != null 
+						&&  password2.getPassword().length != 0) {
+					System.out.print(password2.getPassword().toString());
+					windows1.setVisible(false);
+					initializeWindows2();
+				}
 			}
-			
 		});
+		windows1.getContentPane().add(logIn_button);
+	}
+	
+	void initSmecherie() {
 		
 		JTextField txt = new JTextField(" Text: "); 
 		txt.setEditable(false);
 		txt.setBounds(60,100,100,20);
 		windows1.getContentPane().add(txt);
 		txt.setVisible(false);
+	}
+	
+	void initializeWindows1() {
+		
+		windows1.setBounds(800,300,400,300);
+		windows1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		windows1.getContentPane().setBackground((new Color(100,200,225)));
+		
+		initNickname();
+		initPasswordButton();
+		initLoginButton();
+		initSmecherie();
 		
 		windows1.setVisible(true);
 	}
+	
+	MqttConnectOptions initConnectionOptions() {
+		
+		MqttConnectOptions connOpts = new MqttConnectOptions();
+		connOpts.setCleanSession(true);
+		connOpts.setUserName(nickname.getText());
+		connOpts.setPassword(password2.getPassword());//toCharArray());
+		
+		connOpts.setAutomaticReconnect(true);//Biblioteca va incerca automat sa se reconnecteze la server in cazul unei defectiuni in retea
+		connOpts.setCleanSession(true);//Acesta va elimina mesajele netrimise dintr-o rulare anterioara
+		connOpts.setConnectionTimeout(100);//Timpul de expirare a conexiunii este setat la 100 secunde
+		
+		return connOpts;
+	}
+	
+	void initConnectButton(final MqttConnectOptions connOpts) {
+		
+		JButton connect_button = new JButton("Connect");
+		connect_button.setBounds(100, 30, 120, 20);
+		connect_button.setVisible(true);
+		connect_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//b.connectBroker(mqttClient, clientId, persistence, broker);
+				if (!isConnectedMqtt) {
+					try {
+						mqttClient.connect(connOpts);
+						System.out.println("mqttClient Connected");
+					} catch (MqttSecurityException e1) {
+						e1.printStackTrace();
+					} catch (MqttException e1) {
+						e1.printStackTrace();
+					}
+					isConnectedMqtt = true;
+					// afisare aceasta doar cu un client deja existent
+					nrOfTopic = preferences.getInt("countTopic", 0);
+					System.out.println("la apasarea butonului connect sunt " + nrOfTopic + " topicuri din conectarea anterioara.");
+					for (int k = 0; k <= nrOfTopic; k++) {
+						t[k] = new JTextField("", 100);
+						t[k].setBounds(260, i, 150, 20);
+						t[k].setText(preferences.get("pswTopic" + k, ""));
+						t[k].setVisible(true);
+						t[k].setEditable(false);
+						i += 25;
+						windows2.getContentPane().add(t[k]);
+					}
+				}
+			}
+		});
+		windows2.getContentPane().add(connect_button);
+	}
+	
+	void initNewTopicButton() {
+		
+		JButton newTopic_button = new JButton("Add new topic "); 
+		newTopic_button.setBounds(100, 70, 150, 20);
+		windows2.getContentPane().add(newTopic_button);
+		newTopic_button.setVisible(true);
+		newTopic_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (mqttClient.isConnected()) {
+					System.out.println("clientul este conectat la apasarea butonului 'add new topic'");
+					System.out.println("la apasarea butonului 'add new topic' sunt " + nrOfTopic + " topicuri");
+					txtTopic = JOptionPane.showInputDialog("Topic");
+					if (txtTopic.length() > 0) {
+						nrOfTopic++;
+						preferences.put("pswTopic" + nrOfTopic, txtTopic);
+						t[nrOfTopic] = new JTextField();
+						t[nrOfTopic].setBounds(260, i, 150, 20);
+						t[nrOfTopic].setText(preferences.get("pswTopic" + nrOfTopic, ""));
+						t[nrOfTopic].setEditable(false);
+						windows2.getContentPane().add(t[nrOfTopic]);
+						i += 25;
+						System.out.println("dupa adaugarea unui topic sunt: " + nrOfTopic + " topicuri");
+						//nrOfTopic++;
+						preferences.putInt("countTopic", nrOfTopic);
+					}
+				}
+			}
+		});
+	}
+	
+	void initDeleteElements() {
+		
+		JLabel deleteTopic = new JLabel("Write what topic want to delete:");
+		deleteTopic.setBounds(420, 70, 200, 20);
+		windows2.getContentPane().add(deleteTopic);
+		
+		final JTextField fieldTopicDelete = new JTextField();
+		fieldTopicDelete.setBounds(620, 70, 25, 20);
+		windows2.getContentPane().add(fieldTopicDelete);
+		
+		JButton okDelete = new JButton("ok");
+		okDelete.setBounds(650, 70, 50, 20);
+		windows2.getContentPane().add(okDelete);
+		okDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int indexTopicToDelete = Integer.parseInt(fieldTopicDelete.getText());
+				if (mqttClient.isConnected()) {
+					System.out.println("dupa apasarea butonului ok sunt: " + nrOfTopic + " topicuri");
+					if (nrOfTopic >= 0 && indexTopicToDelete > 0 && indexTopicToDelete-1 <= nrOfTopic && t[indexTopicToDelete-1].isVisible()) {
+						t[indexTopicToDelete-1].setVisible(false);
+						for(int l = indexTopicToDelete-1; l < nrOfTopic - 1; l++) {
+							preferences.put("pswTopic" + l, t[l+1].getText());
+						}
+						nrOfTopic--;
+						if(nrOfTopic < 0) {
+							nrOfTopic = 0;
+						}
+						preferences.putInt("countTopic", nrOfTopic);
+						System.out.println("dupa apasarea butonului ok si elimare sunt: " + nrOfTopic + " topicuri");
+					}
+					else {
+						System.out.println("Nu aveti topic-uri de eliminat, iar nr de topicuri este: " + nrOfTopic);
+					}
+				}
+			}
+		});
+	}
+	
 	void initializeWindows2() {
 		try {
 			mqttClient = new MqttClient(broker, nickname.getText(), persistence);
-			final MqttConnectOptions connOpts = new MqttConnectOptions();
-			connOpts.setCleanSession(true);
-			if (nickname.getText() != null && password2.getPassword() != null) {
-				connOpts.setUserName(nickname.getText());
-				connOpts.setPassword(password2.getPassword());//toCharArray());
-			}
-			connOpts.setAutomaticReconnect(true);//Biblioteca va incerca automat sa se reconnecteze la server în cazul unei defectiuni în retea
-			connOpts.setCleanSession(true);//Acesta va elimina mesajele netrimise dintr-o rulare anterioara
-			connOpts.setConnectionTimeout(100);//Timpul de expirare a conexiunii este setat la 100 secunde
+			
+			final MqttConnectOptions connOpts = initConnectionOptions();
+			
 			System.out.println("Connecting to broker: " + broker);
 			
 			windows2.setBounds(800,300,900,400);
 			windows2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			windows2.getContentPane().setBackground((new Color(100,200,225)));
 		
-			JButton connect_button = new JButton("Connect");
-			connect_button.setBounds(100, 30, 120, 20);
-			windows2.getContentPane().add(connect_button);
-			connect_button.setVisible(true);
-			connect_button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					//b.connectBroker(mqttClient, clientId, persistence, broker);
-					if (!isConnectedMqtt) {
-						try {
-							mqttClient.connect(connOpts);
-							System.out.println("mqttClient Connected");
-						} catch (MqttSecurityException e1) {
-							e1.printStackTrace();
-						} catch (MqttException e1) {
-							e1.printStackTrace();
-						}
-						isConnectedMqtt = true;
-						// afisare aceasta doar cu un client deja existent
-						nrOfTopic = preferences.getInt("countTopic", 0);
-						System.out.println("la apasarea butonului connect sunt " + nrOfTopic + " topicuri din conectarea anterioara.");
-						for (int k = 0; k <= nrOfTopic; k++) {
-							t[k] = new JTextField("", 100);
-							t[k].setBounds(260, i, 150, 20);
-							t[k].setText(preferences.get("pswTopic" + k, ""));
-							t[k].setVisible(true);
-							t[k].setEditable(false);
-							i += 25;
-							windows2.getContentPane().add(t[k]);
-						}
-					}
-				}
-			});
+			initConnectButton(connOpts);
+			initNewTopicButton();
+			initDeleteElements();
 			
-			JButton newTopic_button = new JButton("Add new topic "); 
-			newTopic_button.setBounds(100, 70, 150, 20);
-			windows2.getContentPane().add(newTopic_button);
-			newTopic_button.setVisible(true);
-			newTopic_button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (mqttClient.isConnected()) {
-						System.out.println("clientul este conectat la apasarea butonului 'add new topic'");
-						System.out.println("la apasarea butonului 'add new topic' sunt " + nrOfTopic + " topicuri");
-						txtTopic = JOptionPane.showInputDialog("Topic");
-						if (txtTopic.length() > 0) {
-							nrOfTopic++;
-							preferences.put("pswTopic" + nrOfTopic, txtTopic);
-							t[nrOfTopic] = new JTextField();
-							t[nrOfTopic].setBounds(260, i, 150, 20);
-							t[nrOfTopic].setText(preferences.get("pswTopic" + nrOfTopic, ""));
-							t[nrOfTopic].setEditable(false);
-							windows2.getContentPane().add(t[nrOfTopic]);
-							i += 25;
-							System.out.println("dupa adaugarea unui topic sunt: " + nrOfTopic + " topicuri");
-							//nrOfTopic++;
-							preferences.putInt("countTopic", nrOfTopic);
-						}
-					}
-				}
-			});
-			JLabel deleteTopic = new JLabel("Write what topic want to delete:");
-			deleteTopic.setBounds(420, 70, 200, 20);
-			windows2.getContentPane().add(deleteTopic);
-			
-			final JTextField fieldTopicDelete = new JTextField();
-			fieldTopicDelete.setBounds(620, 70, 25, 20);
-			windows2.getContentPane().add(fieldTopicDelete);
-			
-			JButton okDelete = new JButton("ok");
-			okDelete.setBounds(650, 70, 50, 20);
-			windows2.getContentPane().add(okDelete);
-			okDelete.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					int indexTopicToDelete = Integer.parseInt(fieldTopicDelete.getText());
-					if (mqttClient.isConnected()) {
-						System.out.println("dupa apasarea butonului ok sunt: " + nrOfTopic + " topicuri");
-						if (nrOfTopic >= 0 && indexTopicToDelete > 0 && indexTopicToDelete-1 <= nrOfTopic && t[indexTopicToDelete-1].isVisible()) {
-							t[indexTopicToDelete-1].setVisible(false);
-							for(int l = indexTopicToDelete-1; l < nrOfTopic - 1; l++) {
-								preferences.put("pswTopic" + l, t[l+1].getText());
-							}
-							nrOfTopic--;
-							if(nrOfTopic < 0) {
-								nrOfTopic = 0;
-							}
-							preferences.putInt("countTopic", nrOfTopic);
-							System.out.println("dupa apasarea butonului ok si elimare sunt: " + nrOfTopic + " topicuri");
-						}
-						else {
-							System.out.println("Nu aveti topic-uri de eliminat, iar nr de topicuri este: " + nrOfTopic);
-						}
-					}
-				}
-			});
 		JLabel publishTopic = new JLabel("If you want to publish choose a topic:");
 		publishTopic.setBounds(100, 180, 220, 20);
 		windows2.getContentPane().add(publishTopic);
@@ -259,13 +309,13 @@ public class GraphicsWindows {
 								} 
 								textTopicAndMessages.setText("topic: '" + fieldPublish.getText() + " ' ," + " message: '" + message + " '");
 							//introducerea tuturor topicurilor intr un string
-							/*String[] mesAndTop = new String[nrOfTopic];
+							mesAndTop = new String[nrOfTopic];
 							for (int c = 0; c < nrOfTopic; c++) {
 								mesAndTop[c] = preferences.get("pswTopic" + c, "");
 								System.out.println(mesAndTop[c]);
 								//textTopicAndMessages.insert("topic: '" + txtTopic + " ' ," + " message: '" + message + "'", nrOfRows);
 								//nrOfRows++;
-							}*/
+							}
 					}
 						}
 					//}
